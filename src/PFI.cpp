1edd0c55927fad479423f6fe6f0bcd7ddf16b906
GregVernon <greg.j.vernon@gmail.com>
2017-06-03 15:17:24 -0500

    Added structures for coordinate vars
    First naively implemented structures

--- src/PFI.cpp
+++ src/PFI.cpp
@@ -9,19 +9,41 @@ typedef std::chrono::high_resolution_clock Clock;
 
 // function declarations
 double * linspace(double vMin, double vMax, MKL_INT nInterval);
+struct ParabolicCoordinates
+{
+	int NX, MY;
+	double xmin, xmax, ymin, ymax;
+	double x, y;
+	double dx, dy;
+	double dx2, dy2;
+	double dxx, dyy;
+
+};
+
+struct CartesianCoordinates
+{
+	
+	double bx, by; 
+	double pX, pY;
+	double mu, dmux, dmuy;
+	double eta, detax, detay;
+
+};
 
 void main()
 {
 	auto allStart = Clock::now();
 
-	MKL_INT	NX = 201, MY = 401;
+	int	NX = 201, MY = 401;
 	int i, j;
 	double xmin = -20, xmax = 20, ymin = 1, ymax = 11;
 	double dx, dy;
 	double dx2, dy2;
 	double dxx, dyy;
 	double	*x, *y;
-	
+	struct ParabolicCoordinates * pCOORD = new ParabolicCoordinates;
+	struct CartesianCoordinates * cCOORD = new CartesianCoordinates;
+
 	// Define Parabolic Coordinates
 	x = (double *)mkl_malloc(NX * sizeof(double), 64);
 	y = (double *)mkl_malloc(MY * sizeof(double), 64);
@@ -36,6 +58,21 @@ void main()
 	dy = y[1] - y[0];
 	dy2 = 2 * dy;
 	dyy = dy * dy;
+
+	pCOORD->NX = NX;
+	pCOORD->MY = MY;
+	pCOORD->xmin = xmin;
+	pCOORD->xmax = xmax;
+	pCOORD->ymin = ymin;
+	pCOORD->ymax = ymax;
+	pCOORD->x = *x;
+	pCOORD->y = *y;
+	pCOORD->dx = dx;
+	pCOORD->dx2 = dx2;
+	pCOORD->dxx = dxx;
+	pCOORD->dy = dy;
+	pCOORD->dy2 = dy2;
+	pCOORD->dyy = dyy;
 
 	// Compute Mapping: Parabolic -> Cartesian
 
@@ -62,7 +99,9 @@ double * linspace(double vMin, double vMax, MKL_INT nInterval)
 	return v;
 }
 
-std::tuple<double *, double *, double *, double *, double *, double *, double *, double *, double *, double *> Para2Cart()
+
+
+void Para2Cart(struct ParabolicCoordinates pCOORD)
 {
 	int i, j;
 	double *pX, *pY;
