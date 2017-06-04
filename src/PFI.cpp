a2fa8e9cacab36da217ebd6003550ff8b46c3c7a
GregVernon <greg.j.vernon@gmail.com>
2017-06-04 15:55:03 -0500

    Completed Para2Cart routine
    Haven't verified.

--- src/PFI.cpp
+++ src/PFI.cpp
@@ -25,10 +25,10 @@ struct ParabolicCoordinates
 struct CartesianCoordinates
 {
 	
-	double bx, by; 
+	double *bX, *bY; 
 	double *pX, *pY;
-	double mu, dmux, dmuy;
-	double eta, detax, detay;
+	double *mu, *dmux, *dmuy;
+	double *eta, *detax, *detay;
 
 };
 
@@ -107,38 +107,111 @@ CartesianCoordinates Para2Cart(struct ParabolicCoordinates *pCOORD)
 	pCOORD->x;
 	int NX = pCOORD->NX;
 	int MY = pCOORD->MY;
-	int xmin = pCOORD->xmin;
-	int xmax = pCOORD->xmax;
-	int ymin = pCOORD->ymin;
-	int ymax = pCOORD->ymax;
+	MKL_INT nNODE = NX * MY;
+	double xmin = pCOORD->xmin;
+	double xmax = pCOORD->xmax;
+	double ymin = pCOORD->ymin;
+	double ymax = pCOORD->ymax;
 	double *x = pCOORD->x;
 	double xskew, yskew;
 
 	double *pX, *pY;
+	double *bX, *bY;
+	double *mu, *eta;
+	double *dmux, *dmuy;
+	double *detax, *detay;
+	double *r;
 	
 	struct CartesianCoordinates * cCOORD = new CartesianCoordinates;
 	xskew = 1;
 	yskew = 1;
-	pX = (double *)mkl_malloc(NX * sizeof(double), 64);
-	pY = (double *)mkl_malloc(MY * sizeof(double), 64);
+	pX    =	(double *)mkl_malloc(NX*MY * sizeof(double), 64);
+	pY    =	(double *)mkl_malloc(NX*MY * sizeof(double), 64);
+	dmux  =	(double *)mkl_malloc(NX*MY * sizeof(double), 64);
+	dmuy  =	(double *)mkl_malloc(NX*MY * sizeof(double), 64);
+	detax = (double *)mkl_malloc(NX*MY * sizeof(double), 64);
+	detay = (double *)mkl_malloc(NX*MY * sizeof(double), 64);
+	r	  = (double *)mkl_malloc(NX*MY * sizeof(double), 64);
+	mu    =	(double *)mkl_malloc(NX * sizeof(double), 64);
+	eta   =	(double *)mkl_malloc(MY * sizeof(double), 64);
+	bX    =	(double *)mkl_malloc(NX * sizeof(double), 64);
+	bY    =	(double *)mkl_malloc(NX * sizeof(double), 64);
 
 	for (i = 0; i < NX; ++i)
 	{
 		if (x[i] <= 0.)
 		{
-			pX[i] = pow((-1 / 2) * abs(xmax - xmin)*abs((i - ((NX - 1) / 2) - 1) / ((NX - 1) / 2)), xskew);
+			mu[i] = pow((-1 / 2) * abs(xmax - xmin)*abs((i - ((NX - 1) / 2) - 1) / ((NX - 1) / 2)), xskew);
 		}
 		else if (x[i] > 0.)
 		{
-			pX[i] = pow(( 1 / 2) * abs(xmax - xmin)*abs((i - ((NX - 1) / 2) - 1) / ((NX - 1) / 2)), xskew);
+			mu[i] = pow(( 1 / 2) * abs(xmax - xmin)*abs((i - ((NX - 1) / 2) - 1) / ((NX - 1) / 2)), xskew);
 		}
 	}
 
 	for (j = 0; j < MY; ++j)
 	{
-		pY[j] = ymin + abs(ymax - ymin) * pow(((j - 1) / (MY - 1)), yskew);
+		eta[j] = ymin + abs(ymax - ymin) * pow(((j - 1) / (MY - 1)), yskew);
 	}
 
+	for (j = 0; j < MY; ++j)
+		for (i = 0; i < NX; ++i)
+		{
+			pX[j*NX + i] = 1 / 2 * (pow(mu[i], 2) - pow(eta[j], 2));
+			pY[j*NX + i] = mu[i] * eta[j];
+			r[j*NX + i]  = pow(pX[j*NX + i], 2) + pow(pY[j*NX + i], 2);
+		}
+	
+	for (i = 0; i < NX; ++i)
+	{
+		bX[i] = 1 / 2 * (pow(mu[i], 2) - 1);
+		bY[i] = mu[i];
+	}
+	
+	for (j = 1; j < MY; ++j)
+		for (i = 0; i < NX; ++i)
+		{
+			if (x[i] == 0)
+			{
+				continue; 
+				// SKIP to avoid a "divide by zero" case
+				// Note that this should be an edge case as we would usually expect 
+				// x[i] to be *close to* but not exactly equal to zero
+				// However, x[i] should only be zero when NX is ODD and we check for this case later
+			}
+			else
+			{
+				dmux[j*NX + i]  = (1 / 2) * ((pX[j*NX + i] / r[j*NX + i]) + 1) / (mu[i]);
+				dmuy[j*NX + i]  = (1 / 2) * ((pY[j*NX + i] / r[j*NX + i]) + 0) / (mu[i]);
+				detax[j*NX + i] = (1 / 2) * ((pX[j*NX + i] / r[j*NX + i]) - 1) / (eta[j]);
+				detay[j*NX + i] = (1 / 2) * ((pY[j*NX + i] / r[j*NX + i]) - 0) / (eta[j]);
+			}
+		}
+	
+	if (fmod(NX, 2) != 0)
+	{
+		// NX is ODD
+		i = (NX - 1) / 2;
+		for (j = 1; j < MY; ++j)
+		{
+			dmux[j*NX + i] = (dmux[j*NX + (i - 1)] + dmux[j*NX + (i + 1)]) / 2;
+			dmuy[j*NX + i] = (dmuy[j*NX + (i - 1)] + dmuy[j*NX + (i + 1)]) / 2;
+			detax[j*NX + i] = (detax[j*NX + (i - 1)] + detax[j*NX + (i + 1)]) / 2;
+			detay[j*NX + i] = (detay[j*NX + (i - 1)] + detay[j*NX + (i + 1)]) / 2;
+		}
+	}
+	
+	
+	cCOORD->bX = bX; 
+	cCOORD->bY = bY; 
 	cCOORD->pX = pX;
+	cCOORD->pY = pY;
+	cCOORD->mu = mu;
+	cCOORD->dmux = dmux;
+	cCOORD->dmuy = dmuy;
+	cCOORD->eta = eta;
+	cCOORD->detax = detax;
+	cCOORD->detay = detay;
+
 	return *cCOORD;
 }
